877aacd6ae87a9f5db413235fbf877aac01440b7
Eugyjay3 <147037278+Eugyjay3@users.noreply.github.com>
2023-10-05 08:12:15 -0700

    second new files added

---
+++ crisp.js
@@ -0,0 +1,73 @@
+// const myDoList = document.getElementsByTagName("li");
+// let i;
+// for ( i = 0; i < myDoList.length; i++) {
+//  const span = document.createElement("span");
+//  // let text = document.createTextNode("\u00D7");
+//  // span.className = "close";
+//  span.appendChild(text);
+//  myDoList[i].appendChild(span);
+// }
+
+// const close = document.getElementsByClassName("close");
+//  i;
+//     for ( i = 0; i < close.length; i++) {
+// 	close[i].onclick = function() {
+// 		let div = this.parentElement;
+// 		div.style.display = "none";
+// 	}
+// }
+
+// let list = document.querySelector("ul");
+// list.addEventListener("click", function(ev){
+// 	if (ev.target.tagName ==="li"){
+// 		ev.target.classList.toggle("checked");
+// 	}
+// }, false);
+
+// function newElement() {
+// 	let i = document.createElements("li");
+// 	let inputValue = document.getElementsById("myInput").value;
+// 	let t = document.createTextNode(inputValue);
+// 	li.appendChild(t);
+// 	if(inputValue === ""){
+// 		alert("you must write something");
+//     }
+//     else{
+//     	document.getElementsById("myContent").appendChild(li);
+//     }
+//     document.getElementsById("myInput").value = "";
+
+//     const span = document.createElements("span");
+//     let text = document.createTextNode("\u00D7");
+//     span.className = "close";
+//     span.appendChild(text);
+//     li.appendChild(span);
+
+//     for ( i = 0; i < close.length; i++) {
+// 	close[i].onclick = function(){
+// 		let div = this.parentElement;
+// 		div.style.display = "none";
+// 	}
+//   } 
+// }
+
+// ul.append(li)
+//    const deleteBtn = document.createElement("Btn")
+//    const editBtn = document.createElement("Btn")
+
+//    deleteBtn.textContent = "Delete"
+//    deleteBtn.addEventListener("click", function(){
+// 	deleteTodo(i)
+//    })
+//   li.appendChild(deleteBtn)
+
+//     editBtn.textContent = "Edit"
+//     editBtn.addEventListener("click", function(){
+// 	editTodo(i)
+//     })
+//   li.appendChild(editBtn)
+//     todoList.appendChild(li)
+//   })
+// }
+// addBtn.addEventListener("click", addTodo)
+
